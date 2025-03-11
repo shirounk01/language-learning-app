9258da578163fae422832e10bf628ffd03e3e01d
shirounk01 <raresmihai18@gmail.com>
2025-03-11 16:34:34 +0200

    updated the word model

--- app/myapp/admin.py
+++ app/myapp/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Word)
+# admin.site.register(Word)
+
+
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = (
+        "word",
+        "translation",
+        "part_of_speech",
+        "gender",
+        "conjugation",
+    )
+    list_filter = ("part_of_speech", "gender", "conjugation")
--- app/myapp/models.py
+++ app/myapp/models.py
@@ -4,8 +4,30 @@ from django.db import models
 # Create your models here.
 class Word(models.Model):
 
-    PART_OF_SPEECH = {"N": "Noun", "V": "Verb", "Adj": "Adjective"}
+    PART_OF_SPEECH = {
+        "N": "Noun",
+        "V": "Verb",
+        "Adj": "Adjective",
+        "Adv": "Adverb",
+        "Exp": "Expression",
+    }
 
-    word_id = models.AutoField(primary_key=True)
+    GENDER = {"M": "Masculine", "F": "Feminine", "N": "Neuter"}
+
+    CONJUGATION = {
+        "I": "I",
+        "II": "II",
+        "III": "III",
+        "IV": "IV",
+    }
+
+    id = models.AutoField(primary_key=True)
     word = models.CharField(max_length=200)
-    part_of_speech = models.CharField(max_length=3, choices=PART_OF_SPEECH, default="N")
+    translation = models.CharField(max_length=200)
+    part_of_speech = models.CharField(max_length=3, choices=PART_OF_SPEECH)
+    gender = models.CharField(
+        max_length=10, choices=PART_OF_SPEECH, blank=True, null=True
+    )
+    conjugation = models.CharField(
+        max_length=3, choices=CONJUGATION, blank=True, null=True
+    )
