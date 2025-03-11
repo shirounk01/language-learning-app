15270ee457636d79f8125203974ef893b30af3ed
shirounk01 <raresmihai18@gmail.com>
2025-03-11 14:34:10 +0200

    added baseline changes

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Word(models.Model):
+
+    PART_OF_SPEECH = {"N": "Noun", "V": "Verb", "Adj": "Adjective"}
+
+    word_id = models.AutoField(primary_key=True)
+    word = models.CharField(max_length=200)
+    part_of_speech = models.CharField(max_length=3, choices=PART_OF_SPEECH, default="N")
